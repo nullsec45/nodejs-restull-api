2060f301ec801e09b011b7b6582692186dd678fe
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-03 09:52:49 +0700

    feat : new middlewares

---
+++ src/middlewares/error-middleware.js
@@ -0,0 +1,21 @@
+import { ResponseError } from "../errors/response-errror.js";
+
+const errorMiddleware = async (err, req, res, next) => {
+  if (err instanceof ResponseError) {
+    res
+      .status(err.status)
+      .json({
+        errors: err.message,
+      })
+      .end();
+  } else {
+    res
+      .status(500)
+      .json({
+        errors: err.message,
+      })
+      .end();
+  }
+};
+
+export { errorMiddleware };
