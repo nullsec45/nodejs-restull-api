1872bd740228d4a82fe44c88542d8b8fab51889e
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-04 14:08:26 +0700

    feat : validation login & get

--- src/validations/user-validation.js
+++ src/validations/user-validation.js
@@ -5,3 +5,10 @@ export const registerUserValidation = Joi.object({
   password: Joi.string().max(100).required(),
   name: Joi.string().max(100).required(),
 });
+
+export const loginUserValidation = Joi.object({
+  username: Joi.string().max(100).required(),
+  password: Joi.string().max(100).required(),
+});
+
+export const getUserValidation = Joi.string().max(100).required();
