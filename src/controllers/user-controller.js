ce4c7e69882b92afb9d3e7e4c07068bee069fe37
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-04 14:06:47 +0700

    feat : controller method login & get

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -11,6 +11,32 @@ const register = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const result = await userService.login(req.body);
+    res.status(200).json({
+      data: result,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const get = async (req, res, next) => {
+  try {
+    const username = req.user.username;
+    const result = await userService.get(username);
+
+    res.status(200).json({
+      data: result,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
 export default {
   register,
+  login,
+  get,
 };
