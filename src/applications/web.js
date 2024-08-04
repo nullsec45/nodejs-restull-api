609a534101b2041ede6392d9461cd624be2c2fcf
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-04 14:05:50 +0700

    feat : use web router & auth middleware

--- src/applications/web.js
+++ src/applications/web.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { publicRouter } from "../routes/public-api.js";
 import { errorMiddleware } from "../middlewares/error-middleware.js";
+import { userRouter } from "../routes/api.js";
+import { authMiddleware } from "../middlewares/auth-middleware.js";
 
 export const web = express();
 web.use(express.json());
 
 web.use(publicRouter);
+web.use(authMiddleware);
+
+web.use(userRouter);
+
 web.use(errorMiddleware);
