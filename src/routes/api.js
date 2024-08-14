a28ec80fd806149a5312e695c6123c8f8b74ec13
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-14 23:32:39 +0700

    feat : register address controller in route

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userController from "../controllers/user-controller.js";
 import contactController from "../controllers/contact-controller.js";
+import addressController from "../controllers/address-controller.js";
 
 const userRouter = new express.Router();
 // userAPI
@@ -14,5 +15,9 @@ userRouter.get("/api/contacts/:contactId", contactController.get);
 userRouter.put("/api/contacts/:contactId", contactController.update);
 userRouter.delete("/api/contacts/:contactId", contactController.remove);
 userRouter.get("/api/contacts", contactController.search);
+
+// Address API
+userRouter.post('/api/contacts/:contactId/addresses', addressController.create);
+userRouter.get('/api/contacts/:contactId/addresses/:addressId', addressController.get);
 
 export { userRouter };
