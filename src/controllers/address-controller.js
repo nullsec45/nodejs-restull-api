93cc636160408ae6a2b9fd864a73fd5e18232f01
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-20 20:59:12 +0700

    fea t: update, remove, list controller

--- src/controllers/address-controller.js
+++ src/controllers/address-controller.js
@@ -32,8 +32,56 @@ const get=async(req, res, next) => {
     }
 }
 
+const update=async (req, res,next) => {
+    try{
+        const user=req.user;
+        const request=req.body;
+        const contactId=req.params.contactId;
+
+        const result=await addressService.update(user, contactId, request);
+        res.status(200).json({
+            data:result
+        });
+    }catch(e){
+        next(e);
+    }
+}
+
+const remove=async(req, res, next) => {
+    try{
+        const user=req.user;
+        const contactId=req.params.contactId;
+        const addressId=req.params.addressId;
+
+        await addressService.remove(user, contactId, addressId);
+
+        res.status(200).json({
+            message:"Delete Success"
+        });
+    }catch(e){
+        next(e);
+    }
+}
+
+const list=async(req, res, next) => {
+    try{
+        const user=req.user;
+        const contactId=req.params.contactId;
+
+        const result=await addressService.list(user, contactId);
+
+        res.status(200).json({
+            data:result
+        });
+    }catch(e){
+        next(e);
+    }
+}
 
 export default{
     create,
-    get
+    get,
+    update,
+    remove,
+    list
 }
